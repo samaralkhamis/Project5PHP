f9182d526355cb69a00ab0c0e82d660758c44c92
samaralkhamis <100211042+samaralkhamis@users.noreply.github.com>
2022-05-29 09:15:32 +0300

    done

--- index1.php
+++ index1.php
@@ -14,7 +14,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Amazon Prime Video</title>
+    <title>Kids Toys</title>
     <link rel="stylesheet" href="bootstrap-4.4.1-dist/css/bootstrap.css">
     <link rel="stylesheet" href="bootstrap-4.4.1-dist/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
